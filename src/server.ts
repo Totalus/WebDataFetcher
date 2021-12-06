4753c27506467f0036ef7fa6a4ae71e1c45d81aa
Totalus <34516402+Totalus@users.noreply.github.com>
2021-12-05 20:28:01 -0500

    Adding destinations and other stuff...

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@
 import * as yargs from 'yargs';
 import * as yaml from 'js-yaml';
 import * as fs from 'fs';
+import { Job } from './job';
+import { ConsoleOutput } from './destinations/console';
+import { Output } from './destinations/output';
 
 const argv = yargs
 	.option('config', {
@@ -14,36 +17,70 @@ const argv = yargs
 
 class Scraper {
 
+	jobs: Record<string, Job> = {}
+	destinations: Record<string, Output> = {}
+
 	constructor() {
-
+		this.outputTo = this.outputTo.bind(this);
 	}
 
-	addJob(name: string, options: any) : boolean {
-		return true;
-	}
-	
-	listJobs(): Array<string> {
-		return [];
+	addJob(name: string, options: any) {
+		// Create a cron job
+		console.log(`Adding job '${name}'`);
+
+		if(!!this.jobs[name]) {
+			console.error(`Job '${name}' already exists`);
+			return;
+		}
+
+		let j : Job;
+		try {
+			j = new Job(name, options, this.outputTo);
+			this.jobs[name] = j;
+			j.start();
+		}
+		catch(exception){
+			console.info(`Could not create job ${name} : ${exception}`);
+			return;
+		}
 	}
 	
 	registerDestination(name: string, options: any) {
-		
-	}
-	
-}
+		const {type} = options;
 
+		if(type == 'console') {
+			console.log(`registerDestination '${name}'`);
+			this.destinations[name] = new ConsoleOutput(name);
+		}
+		else {
+			console.error(`Invalid destination type '${type}' for destination '${name}'`)
+		}
+	}
+
+	/** Called when a job wants to write data to a destination */
+	async outputTo(destinationName: string, jobName: string, data: any, options: Record<string, any>) : Promise<boolean> {
+		let target = this.destinations[destinationName];
+		if(!target) {
+			console.error(`Job '${jobName}' trying to output data to a destination that does not exist: '${destinationName}'.`)
+			return false;
+		}
+
+		console.log("Writing data to", destinationName);
+		return target.write(jobName, data, options);
+	}
+}
 
 const scraper = new Scraper()
 
 if(!!argv.config) {
+	console.log('Loading config file', argv.config);
+
 	// Load configuration
 	const doc = yaml.load(fs.readFileSync(argv.config, 'utf-8')) as Record<string, any>;
 
-	console.log(doc);
-
 	// Register destinations
-	if(!!doc?.destination) {
-		Object.entries(doc.destination).forEach(([name, opt]) => {
+	if(!!doc?.destinations) {
+		Object.entries(doc.destinations).forEach(([name, opt]) => {
 			scraper.registerDestination(name, opt);
 		});
 	}
@@ -56,3 +93,9 @@ if(!!argv.config) {
 	}
 }
 
+// function shutdown() {
+// 	scraper.shutdown()
+// }
+
+// process.on('SIGTERM', shutdown);
+// process.on('SIGINT', shutdown);
