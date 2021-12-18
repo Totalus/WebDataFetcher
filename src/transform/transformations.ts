736dbed280c766df3de22317131d5a91b7f46be1
Totalus <34516402+Totalus@users.noreply.github.com>
2021-12-18 18:40:00 -0500

    Improve transformations. Add more transformations.

--- src/transform/transformations.ts
+++ src/transform/transformations.ts
@@ -1,55 +1,76 @@
-import { Type } from "js-yaml";
 
-export interface TypeCastTransformOptions {
-}
+/**
+ * Replace
+ */
 
-export interface RegexTransformOptions {
-	pattern: string,
-	index?: number
-}
-
-export interface ReplaceTransformOptions {
+interface ReplaceOptions {
 	replaceWith: string,
 	search: string
 }
 
-
-function typecastTransform(options: TypeCastTransformOptions, value: any) : any {
-	return Number(value);
+function replace(options: ReplaceOptions, text: string) : string {
+	if(!options?.search) throw new Error("Missing 'search' option");
+	if(options.replaceWith === undefined || options.replaceWith === null) throw new Error("Missing 'replaceWith' option");
+	return text.replaceAll(options.search, options.replaceWith);
 }
 
-function regexTransform({pattern, index}: RegexTransformOptions, text: string) : string | null {
-	let re = new RegExp(pattern);
+/**
+ * RegexReplace
+ */
 
-	let res = re.exec(text);
-	console.log('index', index, res, pattern);
-
-	if(!res)
-		return null;
-
-	if(index !== undefined && res.length > index + 1)
-		return res[index];
-	else if(index === undefined && res.length > 1)
-		return res[1];
-	else
-		return null;
+interface RegexReplaceOptions {
+	pattern: string,
+	output: string,
 }
 
-function replaceTransform({search, replaceWith}: ReplaceTransformOptions, text: string) : string {
-	return text.replace(search, replaceWith);
+function regexReplace(options: RegexReplaceOptions, value: string) : string {
+	if(!options?.pattern) throw new Error("Missing 'pattern' option");
+	if(!options.output) throw new Error("Missing 'output' option");
+	return value.replace(new RegExp(options.pattern), options.output);
 }
 
+/**
+ * TypeCast
+ */
+
+interface TypecastOptions {
+	targetType: string
+}
+
+function typecast(options: TypecastOptions, value: any) : any {
+	
+	if(!options?.targetType) throw new Error("Missing 'targetType' option");
+
+	switch(options.targetType) {
+		case 'number': return Number(value);
+		case 'string': return String(value);
+		default: return null;
+	}
+}
+
+
+/**
+ * TextToJson
+ */
+
+interface TextToJsonOptions {
+}
+
+function textToJson(options: TextToJsonOptions, value: any) : any {
+	return JSON.parse(value);
+}
+
+/**
+ * Function to apply a transformation on a value, by its name
+ */
 export function applyTransformation(name: string, options: Record<string, any>, value: any) : any {
 	
 	console.log("Applying transformation", name, "on value", value);
 	switch(name) {
-		case 'regex':
-			return regexTransform(options as RegexTransformOptions, value);
-
-		case 'replace':
-			return replaceTransform(options as ReplaceTransformOptions, value);
-
-		case 'typecast':
-			return typecastTransform(options as TypeCastTransformOptions, value);
+		case 'regex_replace': return regexReplace(options as RegexReplaceOptions, value);
+		case 'replace': return replace(options as ReplaceOptions, value);
+		case 'typecast': return typecast(options as TypecastOptions, value);
+		case 'text_to_json': return textToJson(options as TextToJsonOptions, value);
+		default: throw new Error(`Unknown transformation '${name}'`);
 	}
 }
