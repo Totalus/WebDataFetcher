6e6c6bb0d60c14ee9895533a253d2240da2ba604
Raphael <raph.nev@gmail.com>
2021-12-19 10:30:51 -0500

    Adding some colors to the logging

--- src/logging.ts
+++ src/logging.ts
@@ -1,4 +1,23 @@
 
+// Color codes : https://stackoverflow.com/a/41407246
+const Reset = "\x1b[0m"
+const Bright = "\x1b[1m"
+const Dim = "\x1b[2m"
+const Reverse = "\x1b[7m"
+const Hidden = "\x1b[8m"
+ 
+const FgRed = "\x1b[31m"
+const FgYellow = "\x1b[33m"
+const FgBlue = "\x1b[34m"
+ 
+const BgBlack = "\x1b[40m"
+const BgRed = "\x1b[41m"
+const BgGreen = "\x1b[42m"
+const BgYellow = "\x1b[43m"
+const BgBlue = "\x1b[44m"
+const BgMagenta = "\x1b[45m"
+const BgCyan = "\x1b[46m"
+const BgWhite = "\x1b[47m"
 
 
 export const logger = {
@@ -29,25 +48,28 @@ export const logger = {
 
 	error: (scope: string, message: string) => {
 		if(logger.logLevel >= 1)
-		logger.log("ERROR", scope, message);
+			logger.log(`${BgRed}ERROR${Reset}`, scope, message);
 	},
 	
 	info: (scope: string, message: string) => {
 		if(logger.logLevel >= 3)
-		logger.log("INFO ", scope, message);
+			logger.log(`${BgWhite}INFO ${Reset}`, scope, message);
 	},
 	
 	debug: (scope: string, message: string) => {
 		if(logger.logLevel >= 4)
-			logger.log("DEBUG", scope, message);
+			logger.log(`DEBUG`, scope, message);
 	},
 
 	warning: (scope: string, message: string) => {
-		if(logger.logLevel >= 2)			
-			logger.log("WARN ", scope, message);
+		if(logger.logLevel >= 2)
+			logger.log(`${BgYellow}WARN ${Reset}`, scope, message);
 	},
 
 	log: (level: string, scope: string, message: string) => {
-		console.log(`${level} [${scope}] ${message}`);
+		if(!!scope)
+			console.log(`${level} (${scope}) ${message}`);
+		else
+			console.log(`${level} ${message}`);
 	}
 }
