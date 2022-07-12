5f96fad598e6b126528dc6f0ff480905276ee09b
Totalus <34516402+Totalus@users.noreply.github.com>
2022-07-11 22:12:23 -0400

    Throw error if text is undefined in replace transformation

--- src/transform/transformations.ts
+++ src/transform/transformations.ts
@@ -11,6 +11,7 @@ interface ReplaceOptions {
 }
 
 function replace(options: ReplaceOptions, text: string) : string {
+	if(!text) throw new Error("Text undefined");
 	if(!options?.search) throw new Error("Missing 'search' option");
 	if(options.replaceWith === undefined || options.replaceWith === null) throw new Error("Missing 'replaceWith' option");
 	return text.replaceAll(options.search, options.replaceWith);
